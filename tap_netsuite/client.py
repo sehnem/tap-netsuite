e4ba4fcd730c6a3f500d7f90d96a49c1b5e21a44
hg-ramosaj <134003040+hg-ramosaj@users.noreply.github.com>
2023-06-16 14:47:12 -0500

    Add body fields (#4)
    * changed custom field values to take in array, basic types, and single objects
    
    * add flag to extend body fields (expenses, etc)

--- tap_netsuite/client.py
+++ tap_netsuite/client.py
@@ -119,7 +119,7 @@ class NetsuiteStream(Stream):
         if include_search_preferences:
             search_preferences = self.search_client("SearchPreferences")
             preferences = {
-                "bodyFieldsOnly": True,
+                "bodyFieldsOnly": False,
                 "pageSize": self.page_size,
                 "returnSearchColumns": True,
             }
@@ -279,13 +279,30 @@ class NetsuiteStream(Stream):
                                             },
                                             "typeId": { 
                                                 "type":["string","null"]
-                                            },
+                                            }
                                         },
-                                        "type":["object","null"]
+                                        "type":"object"
                                     },
                                 },
                                 {
-                                    "type":["string","boolean","number","integer","null"]
+                                    "type":"object",
+                                     "properties": { 
+                                            "internalId": { 
+                                                "type":["string","null"]
+                                            },
+                                            "externalId": { 
+                                                "type":["string","null"]
+                                            },
+                                            "name": { 
+                                                "type":["string","null"]
+                                            },
+                                            "typeId": { 
+                                                "type":["string","null"]
+                                            }
+                                    }
+                                }, 
+                                {
+                                    "type":["string","boolean","integer","number"]
                                 }
 
                             ]
