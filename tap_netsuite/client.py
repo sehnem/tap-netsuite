2fcced7966003fd05115c4e0935d2f399604895c
Josué M. Sehnem <josue@sehnem.com>
2023-06-27 12:08:07 -0300

    Sdk connector (#2)
    * New file structure and fixes

--- tap_netsuite/client.py
+++ tap_netsuite/client.py
@@ -6,33 +6,27 @@ import hmac
 import random
 from datetime import datetime
 from decimal import Decimal
-from time import time
-from typing import Iterable, Optional
 
-import backoff
 from backports.cached_property import cached_property
 from memoization import cached
 from pendulum import parse
-from singer import Transformer
 from singer_sdk import typing as th
-from singer_sdk.exceptions import FatalAPIError, RetriableAPIError
-from singer_sdk.streams import Stream
-from zeep import Client
-from zeep.cache import SqliteCache
-from zeep.helpers import serialize_object
-from zeep.transports import Transport
 
-from tap_netsuite.constants import REPLICATION_KEYS, RETRYABLE_ERRORS
-from tap_netsuite.exceptions import TypeNotFound
+from tap_netsuite.constants import REPLICATION_KEYS
+from tap_netsuite.paginator import NetsuitePaginator
+from tap_netsuite.streams.soap import SOAPStream
 
 
-class NetsuiteStream(Stream):
+class NetsuiteStream(SOAPStream):
     """Stream class for Netsuite streams."""
 
-    page_size = 500
     primary_keys = ["internalId"]
-    search_type_name = None
-    valid_requests = ["getAllResult", "searchResult", "searchMoreWithIdResult"]
+    retry_statuses = [
+        "ACCT_TEMP_UNAVAILABLE",
+        "BILL_PAY_STATUS_UNAVAILABLE",
+        "BILLPAY_SRVC_UNAVAILBL",
+        "PAYROLL_IN_PROCESS",
+    ]
 
     @cached_property
     def account(self):
@@ -46,41 +40,19 @@ class NetsuiteStream(Stream):
         )
 
     @cached_property
-    def datacenter_url(self):
+    def proxy_url(self):
+        return "{urn:platform_2022_2.webservices.netsuite.com}NetSuiteBinding"
+
+    @cached_property
+    def service_url(self):
         return (
             f"https://{self.account}.suitetalk.api.netsuite.com/"
-            "services/NetSuitePort_2022_2"
+            "/services/NetSuitePort_2022_2"
         )
-
-    @property
-    def client(self):
-        if self.config["cache_wsdl"]:
-            path = "cache.db"
-            timeout = 2592000
-            cache = SqliteCache(path=path, timeout=timeout)
-            transport = Transport(cache=cache)
-            return Client(self.wsdl_url, transport=transport)
-        return Client(self.wsdl_url)
 
     @cached_property
     def service_proxy(self):
-        proxy_url = "{urn:platform_2022_2.webservices.netsuite.com}NetSuiteBinding"
-        return self.client.create_service(proxy_url, self.datacenter_url)
-
-    def search_client(self, type_name):
-        for ns_type in self.client.wsdl.types.types:
-            if ns_type.name and ns_type.name == type_name:
-                return ns_type
-        raise TypeNotFound(f"Type {type_name} not found in WSDL")
-
-    @cached_property
-    def ns_type(self):
-        return self.search_client(self.name)
-
-    @cached_property
-    def search_type(self):
-        search_type_name = self.search_type_name or self.name + "SearchBasic"
-        return self.search_client(search_type_name)
+        return self.client.create_service(self.proxy_url, self.service_url)
 
     def generate_token_passport(self):
         consumer_key = self.config["consumer_key"]
@@ -100,10 +72,10 @@ class NetsuiteStream(Stream):
         dig = hashed_value.digest()
         signature_value = base64.b64encode(dig).decode()
 
-        passport_signature = self.search_client("TokenPassportSignature")
+        passport_signature = self.get_client_type("TokenPassportSignature")
         signature = passport_signature(signature_value, algorithm="HMAC-SHA256")
 
-        passport = self.search_client("TokenPassport")
+        passport = self.get_client_type("TokenPassport")
         return passport(
             account=account,
             consumerKey=consumer_key,
@@ -113,136 +85,23 @@ class NetsuiteStream(Stream):
             signature=signature,
         )
 
-    def build_headers(self, include_search_preferences: bool = False):
-        soapheaders = {}
-        soapheaders["tokenPassport"] = self.generate_token_passport()
-        if include_search_preferences:
-            search_preferences = self.search_client("SearchPreferences")
-            preferences = {
-                "bodyFieldsOnly": True,
-                "pageSize": self.page_size,
-                "returnSearchColumns": True,
-            }
-            soapheaders["searchPreferences"] = search_preferences(**preferences)
-        return soapheaders
-
-    @backoff.on_exception(backoff.expo, RetriableAPIError, max_tries=5, factor=2)
-    def request(self, name, *args, **kwargs):
-        method = getattr(self.service_proxy, name)
-        # call the service:
-        is_search = name == "search"
-        headers = self.build_headers(include_search_preferences=is_search)
-
-        request_start_time = time()
-        response = method(*args, _soapheaders=headers, **kwargs)
-        request_duration = time() - request_start_time
-
-        response_body_attrs = list(vars(response.body)["__values__"].keys())
-        request_type = next(k for k in response_body_attrs if k in self.valid_requests)
-
-        result = getattr(response.body, request_type)
-
-        if hasattr(result, "totalRecords"):
-            page_size = result.totalRecords
-        elif result.totalPages == result.pageIndex:
-            page_size = result.totalRecords - (result.pageIndex - 1) * result.pageSize
-        else:
-            page_size = result.pageSize
-
-        request_status = "SUCCESS" if result.status.isSuccess else "ERROR"
-        extra_tags = dict(page_size=page_size)
-        metric = {
-            "type": "timer",
-            "metric": "request_duration",
-            "value": round(request_duration, 4),
-            "tags": {
-                "object": self.name,
-                "status": request_status,
-            },
-        }
-        self._write_metric_log(metric=metric, extra_tags=extra_tags)
-
-        self.validate_response(result)
-        return result
-
-    def get_all_records(self, context):
-        type_name = self.name[0].lower() + self.name[1:]
-        get_all_record = self.search_client("GetAllRecord")
-        record = get_all_record(recordType=type_name)
-        response = self.request("getAll", record=record)
-
-        records = response["recordList"]["record"]
-        return serialize_object(records)
-
-    @cached
-    def get_starting_time(self, context):
-        start_date = self.config.get("start_date")
-        if start_date:
-            start_date = parse(start_date)
-        rep_key = self.get_starting_timestamp(context)
-        return rep_key or start_date
-
-    def get_all_paginated(self, context):
-        # apply filters
-        search_type = self.search_type()
-        rk = self.replication_key
-        start_date = self.get_starting_time(context)
-        if start_date and rk and hasattr(search_type, rk):
-            search_date = self.search_client("SearchDateField")
-            search_date = search_date(searchValue=start_date, operator="onOrAfter")
-            setattr(search_type, rk, search_date)
-        if getattr(search_type, "recordType", None):
-            search_string = self.search_client("SearchStringField")
-            search_type.recordType = search_string(
-                searchValue=self.name, operator="contains"
-            )
-
-        # request records
-        result = self.request("search", searchRecord=search_type)
-        total_pages = result.totalPages
-        page_index = result.pageIndex
-        search_id = result.searchId
-
-        if total_pages == 0:
-            return []
-
-        for record in result["recordList"]["record"]:
-            yield serialize_object(record)
-
-        while total_pages > page_index:
-            next_page = page_index + 1
-            result = self.request(
-                "searchMoreWithId", searchId=search_id, pageIndex=next_page
-            )
-            page_index = result.pageIndex
-
-            for record in result["recordList"]["record"]:
-                yield serialize_object(record)
-
-    def get_records(self, context: Optional[dict]) -> Iterable[dict]:
-        if self.record_type == "GetAllRecordType":
-            response = self.get_all_records(context)
-        elif self.record_type == "SearchRecordType":
-            response = self.get_all_paginated(context)
-
-        def pre_hook(data, _, schema):
-            if schema.get("format") == "date-time" and data:
-                data = data.isoformat()
-            return data
-
-        with Transformer(pre_hook=pre_hook) as transformer:
-            for record in response:
-                yield transformer.transform(record, self.schema)
+    @cached_property
+    def catalog_dict(self):
+        if getattr(self._tap, "input_catalog"):
+            catalog = self._tap.input_catalog.to_dict()
+            return catalog["streams"]
+        return {}
 
     @cached_property
     def schema(self):
         if getattr(self._tap, "input_catalog"):
-            streams = self._tap.input_catalog.to_dict()
-            streams = (s for s in streams["streams"] if s["tap_stream_id"] == self.name)
-            stream_catalog = next(streams, None)
+            streams = self.catalog_dict
+            stream = (s for s in streams if s["tap_stream_id"] == self.name)
+            stream_catalog = next(stream, None)
             if stream_catalog:
                 return stream_catalog["schema"]
-        properties = self.unwrap_zeep(self.ns_type)
+        soap_type_obj = self.get_client_type(self.name)
+        properties = self.unwrap_zeep(soap_type_obj)
         replication_key = next(
             (p for p in properties if p.name.lower() in REPLICATION_KEYS), None
         )
@@ -284,24 +143,81 @@ class NetsuiteStream(Stream):
 
         for field_name, type_obj in fields_list:
             schema_type = self.extract_type(type_obj)
-            if type_obj.name in ["nullFieldList"]:
-                continue
             if schema_type:
                 property = th.Property(field_name, schema_type)
                 properties.append(property)
         return properties
 
-    def validate_response(self, result) -> None:
-        """Validate zeep response."""
-        if not result.status.isSuccess:
-            status = result.status.statusDetail[0]
-            if status.code in RETRYABLE_ERRORS:
-                msg = self.response_error_message(status)
-                raise RetriableAPIError(msg, status)
-            else:
-                msg = self.response_error_message(status)
-                raise FatalAPIError(msg)
 
-    def response_error_message(self, status) -> str:
-        """Build error message for invalid http statuses."""
-        return f'{status.code} error for {self.name}: "{status.message}"'
+class NetsuiteGetAllStream(NetsuiteStream):
+    """Stream class for Netsuite streams."""
+
+    soap_service = "getAll"
+    records_jsonpath = "$.body.getAllResult.recordList.record[*]"
+    status_jsonpath = "$.body.getAllResult.status"
+    paginator = NetsuitePaginator
+
+    @property
+    def soap_headers(self):
+        soapheaders = {}
+        soapheaders["tokenPassport"] = self.generate_token_passport()
+        return soapheaders
+
+    def prepare_request_body(self, context, next_page_token=None):
+        get_all_record = self.get_client_type("GetAllRecord")
+        record = get_all_record(recordType=self.soap_type)
+        return dict(record=record)
+
+
+class NetsuiteSearchPaginatedStream(NetsuiteStream):
+    """Stream class for Netsuite streams."""
+
+    page_size = 500
+    soap_service = "search"
+    records_jsonpath = "$.body.searchResult.recordList.record[*]"
+    status_jsonpath = "$.body.searchResult.status"
+    search_type_name = None
+
+    def get_new_paginator(self):
+        return NetsuitePaginator(0)
+
+    @property
+    def soap_headers(self):
+        soapheaders = {}
+        soapheaders["tokenPassport"] = self.generate_token_passport()
+        search_preferences = self.get_client_type("SearchPreferences")
+        preferences = {
+            "bodyFieldsOnly": True,
+            "pageSize": self.page_size,
+            "returnSearchColumns": True,
+        }
+        soapheaders["searchPreferences"] = search_preferences(**preferences)
+        return soapheaders
+
+    @cached
+    def get_starting_time(self, context):
+        start_date = self.config.get("start_date")
+        if start_date:
+            start_date = parse(start_date)
+        rep_key = self.get_starting_timestamp(context)
+        return rep_key or start_date
+
+    def prepare_request_body(self, context, next_page_token=None):
+        if next_page_token.get("searchId"):
+            self.soap_service = "searchMoreWithId"
+            return next_page_token
+
+        search_type_name = self.search_type_name or self.name + "SearchBasic"
+        search_type = self.get_client_type(search_type_name)()
+        rk = self.replication_key
+        start_date = self.get_starting_time(context)
+        if start_date and rk and hasattr(search_type, rk):
+            search_date = self.get_client_type("SearchDateField")
+            search_date = search_date(searchValue=start_date, operator="onOrAfter")
+            setattr(search_type, rk, search_date)
+        if getattr(search_type, "recordType", None):
+            search_string = self.get_client_type("SearchStringField")
+            search_type.recordType = search_string(
+                searchValue=self.name, operator="contains"
+            )
+        return dict(searchRecord=search_type)
---
+++ tap_netsuite/paginator.py
@@ -0,0 +1,35 @@
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.pagination import BaseAPIPaginator
+from zeep.helpers import serialize_object
+
+
+class NetsuitePaginator(BaseAPIPaginator):
+    _search_id = None
+
+    def get_next(self, response):
+        response = serialize_object(response)
+
+        self._value = next(
+            extract_jsonpath("$.body.searchResult.pageIndex", input=response)
+        )
+        self._page_count = next(
+            extract_jsonpath("$.body.searchResult.totalPages", input=response)
+        )
+        self._search_id = next(
+            extract_jsonpath("$.body.searchResult.searchId", input=response)
+        )
+        self._finished = self._value >= self._page_count
+
+        if self._finished:
+            return None
+
+        return self._value + 1
+
+    @property
+    def current_value(self):
+        """Get the current pagination value.
+
+        Returns:
+            Current page value.
+        """
+        return dict(searchId=self._search_id, pageIndex=self._value)
